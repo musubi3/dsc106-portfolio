23ddb32974a296a54fd1cd5b604ae8399295d9e0
Justin Lee <ju3tinlee@gmail.com>
2025-11-20 14:55:53 -0800

    Added home folder to hold JS and CSS for homepage

--- home/home.js
+++ home/home.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from '../global.js';
 
 async function displayLatestProjects() {
   const projects = await fetchJSON('./lib/projects.json');
